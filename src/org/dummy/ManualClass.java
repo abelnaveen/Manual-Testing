988aa35b5c0b2113cc2a6311d56b315091023bf2
abelnaveen <abelnaveen@gmail.com>
2023-02-22 17:52:58 +0530

    message

---
+++ src/org/dummy/ManualClass.java
@@ -0,0 +1,14 @@
+package org.dummy;
+
+public class ManualClass {
+
+public static void main(String[] args) {
+	
+	System.out.println(1);
+	System.out.println(2);
+	System.out.println(3);
+	System.out.println("Naveen Manual testing class");
+
+}
+
+}
